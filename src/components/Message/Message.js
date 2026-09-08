e5abcb238e4bf4f374c2c5b4f2b6ba1563794ca2
AGPTCollabLab <agpt.collablab@gmail.com>
2026-09-08 00:48:03 -0500

    user message screen reader fix

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -187,8 +187,9 @@ export const Message = ({ role, content, onAnnotate }) => {
     <article
       className={`bg-gray-600 p-4 ${user?.picture ? 'ml-12' : ''}`}
       data-message-role="user"
+      tabIndex={0}
+      aria-label={'Your message: ${content}'}
     >
-      <span className="sr-only">Your message:</span>
       <div className="flex">
         <div
           className="w-[30px] flex flex-col justify-start items-center mr-4"
